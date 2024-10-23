b3014769af25f72a44aeb5079075d1cdc0f05405
Orealba Soriano <orealbas@gmail.com>
2024-10-23 17:23:12 +0200

    ending level 1

--- js/checkout.js
+++ js/checkout.js
@@ -1,11 +1,12 @@
 // Exercise 6
-function validate() {
+function validate(e) {
+  e.preventDefault();
   let error = 0;
   // Get the input fields
   let fName = document.getElementById('fName');
   let fEmail = document.getElementById('fEmail');
   let fLastN = document.getElementById('fLastN');
-  let fAdress = document.getElementById('fAdress');
+  let fAddress = document.getElementById('fAddress');
   let fPassword = document.getElementById('fPassword');
   let fPhone = document.getElementById('fPhone');
 
@@ -13,7 +14,7 @@ function validate() {
   let errorName = document.getElementById('errorName');
   let errorEmail = document.getElementById('errorEmail');
   let errorLastN = document.getElementById('errorLastN');
-  let errorAdress = document.getElementById('errorAdress');
+  let errorAddress = document.getElementById('errorAddress');
   let errorPassword = document.getElementById('errorPassword');
   let errorPhone = document.getElementById('errorPhone');
 
@@ -24,59 +25,79 @@ function validate() {
 
   // Validate fields entered by the user: name, phone, password, and email
 
-  if (fName.value == '' || fName.value.length < 3 || !fName.value.match(letters)) {
+  if (
+    fName.value == '' ||
+    fName.value.length < 3 ||
+    !fName.value.match(letters)
+  ) {
     error++;
     fName.classList.add('is-invalid');
     errorName.style.display = 'block';
-	errorName.textContent = "El nombre debe tener mínimo 3 caracteres y solo deben ser letras"
+    errorName.textContent =
+      'El nombre debe tener mínimo 3 caracteres y solo deben ser letras';
   } else {
     fName.classList.remove('is-invalid');
-    errorName.style.display('none');
+    errorName.style.display = 'none';
   }
 
-  if (fEmail.value == '' || fEmail.value.length < 3 || !fEmail.value.match(letters)) {
+  if (fEmail.value == '' || !fEmail.value.match(emailPattern)) {
     error++;
     fEmail.classList.add('is-invalid');
     errorEmail.style.display = 'block';
+    errorEmail.textContent =
+      'El email debe tener un formato válido (ej: user@example.com)';
   } else {
     fEmail.classList.remove('is-invalid');
-    errorEmail.style.display('none');
+    errorEmail.style.display = 'none';
   }
 
-  if (fLastN.value == '') {
+  if (
+    fLastN.value == '' ||
+    fLastN.value.length < 3 ||
+    !fLastN.value.match(letters)
+  ) {
     error++;
     fLastN.classList.add('is-invalid');
     errorLastN.style.display = 'block';
+    errorLastN.textContent =
+      'El apellido debe tener al menos 3 caracteres y solo letras';
   } else {
     fLastN.classList.remove('is-invalid');
-    errorLastN.style.display('none');
+    errorLastN.style.display = 'none';
   }
-  if (fAdress.value == '') {
+
+  if (fAddress.value == '' || fAddress.value.length < 3) {
     error++;
-    fAdress.classList.add('is-invalid');
-    errorAdress.style.display = 'block';
+    fAddress.classList.add('is-invalid');
+    errorAddress.style.display = 'block';
+    errorAddress.textContent = 'La dirección debe tener al menos 3 caracteres';
   } else {
-    fAdress.classList.remove('is-invalid');
-    errorAdress.style.display('none');
+    fAddress.classList.remove('is-invalid');
+    errorAddress.style.display = 'none';
   }
-  if (fPassword.value == '') {
+
+  if (fPassword.value == '' || !fPassword.value.match(lettersandNumbers)) {
     error++;
     fPassword.classList.add('is-invalid');
     errorPassword.style.display = 'block';
+    errorPassword.textContent =
+      'La contraseña debe tener entre 4 y 8 caracteres, e incluir letras y números';
   } else {
     fPassword.classList.remove('is-invalid');
-    errorPassword.style.display('none');
+    errorPassword.style.display = 'none';
   }
-  if (fPhone.value == '') {
+
+  if (fPhone.value == '' || !fPhone.value.match(numbers)) {
     error++;
     fPhone.classList.add('is-invalid');
     errorPhone.style.display = 'block';
+    errorPhone.textContent = 'El teléfono sólo debe contener números';
   } else {
     fPhone.classList.remove('is-invalid');
-    errorPhone.style.display('none');
+    errorPhone.style.display = 'none';
   }
   if (error > 0) {
-    alert('Por favor, completa todos los campos.');
+    alert('Por favor, coloque de forma correcta los campos en rojo.');
   } else {
     alert('Formulario completado correctamente.');
   }
